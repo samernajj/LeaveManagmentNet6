a8a8f6c68a2c22835102efe5eb8181a58cfa5873
Samer Najjar <samer.najjar@maf.co.ae>
2022-01-10 15:31:26 +0200

    first commit

---
+++ MyFirstProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirstProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
